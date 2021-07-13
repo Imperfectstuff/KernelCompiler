5952677298d60ae92422c4bec5afc878f22ed727
Imperfect <84269993+ImperfectNiBos@users.noreply.github.com>
2021-07-13 21:11:34 +0800

    Create check-gcc.sh

---
+++ check-gcc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Copyright (C) 2021 a xyzprjkt property
+
+# Checking environtment
+# Warning !! Dont Change anything there without known reason.
+echo ================================================
+echo xKernelCompiler
+echo version : rev1.0 - gaspoll
+echo ================================================
+echo BUILDER NAME = ${KBUILD_BUILD_USER}
+echo BUILDER HOSTNAME = ${KBUILD_BUILD_HOST}
+echo DEVICE_DEFCONFIG = ${DEVICE_DEFCONFIG}
+echo GCC_ROOTDIR = ${GCC_ROOTDIR}
+echo GCC32_ROOTDIR = ${GCC32_ROOTDIR}
+echo KERNEL_ROOTDIR = ${KERNEL_ROOTDIR}
+echo ================================================
