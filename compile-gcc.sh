ea8792b80d01217d65dd967b974ff022e14c5eba
Imperfect <84269993+ImperfectNiBos@users.noreply.github.com>
2021-07-13 22:35:01 +0800

    Update compile-gcc.sh

--- compile-gcc.sh
+++ compile-gcc.sh
@@ -5,6 +5,6 @@
 # Warning !! Dont Change anything there without known reason.
 cd ${KERNEL_ROOTDIR}
                 export CROSS_COMPILE=${GCC_ROOTDIR}/gcc-arm64/bin/aarch64-elf-
-		export CROSS_COMPILE_ARM32=${GCC_ROOTDIR}/gcc-arm/bin/arm-eabi-
+		export CROSS_COMPILE_ARM32=${GCC32_ROOTDIR}/gcc-arm/bin/arm-eabi-
 		make O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
 		make -j$(nproc --all) O=out ARCH=arm64
