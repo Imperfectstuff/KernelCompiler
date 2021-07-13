9b99300fd24b7c2c8b853dfd6953ab3b921bda87
Imperfect <84269993+ImperfectNiBos@users.noreply.github.com>
2021-07-13 21:14:04 +0800

    Create compile-gcc.sh

---
+++ compile-gcc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Copyright (C) 2021 a xyzprjkt property
+
+# Warning !! Dont Change anything there without known reason.
+cd ${KERNEL_ROOTDIR}
+                export CROSS_COMPILE=${GCC_ROOTDIR}/gcc-arm64/bin/aarch64-elf-
+		export CROSS_COMPILE_ARM32=${GCC_ROOTDIR}/gcc-arm/bin/arm-eabi-
+		make O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
+		make -j$(nproc --all) O=out ARCH=arm64
