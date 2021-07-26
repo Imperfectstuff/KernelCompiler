053fded56d06aa81623be31dc6baf0dd709824da
Imperfect <84269993+ImperfectNiBos@users.noreply.github.com>
2021-07-26 10:16:16 +0800

    Update target.sh

--- target.sh
+++ target.sh
@@ -5,6 +5,6 @@
 # Main
 KERNEL_ROOTDIR=~/work/UbuntuSSH/UbuntuSSH/begonia # IMPORTANT ! Fill with your kernel source root directory.
 DEVICE_DEFCONFIG=begonia_user_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
-CLANG_ROOTDIR=~/work/UbuntuSSH/UbuntuSSH/azure # IMPORTANT! Put your clang directory here.
+CLANG_ROOTDIR=~/work/UbuntuSSH/UbuntuSSH/proton # IMPORTANT! Put your clang directory here.
 export KBUILD_BUILD_USER=Imperfect # Change with your own name or else.
 export KBUILD_BUILD_HOST=GengKapak # Change with your own hostname.
